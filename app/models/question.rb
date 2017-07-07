5e1513b25e8c0271e45d59d7eacf9f38dcf5a157
Nina <ninariccimarie@gmail.com>
2017-07-07 22:05:31 +0800

    delete association with Quiz

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,4 @@
 class Question < ApplicationRecord
-    belongs_to :quiz, class_name: "Quiz"
     enum difficulty_level: [:easy, :medium, :hard]
     validates_presence_of :question, :answer, :difficulty_level
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  let(:quiz) { Quiz.create(id: 1) }
   subject { 
     described_class.new(question: "What type of animal is a dog?", answer: "mammal",
-                        difficulty_level: 1, quiz_id: quiz.id) 
+                        difficulty_level: 1) 
   }
 
   it "is valid with valid attributes" do
@@ -24,9 +23,5 @@ RSpec.describe Question, type: :model do
   it "is invalid without a difficulty level" do
     subject.difficulty_level = nil
     expect(subject).to_not be_valid
-  end
-
-  describe "Associations" do
-    it { should belong_to :quiz }
   end
 end
