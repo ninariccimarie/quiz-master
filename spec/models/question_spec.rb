76a3799f01b45ed030f21f9e279a87065ab62e04
Nina <ninariccimarie@gmail.com>
2017-07-09 00:43:54 +0800

    add is_correct? when answer is number test case

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,27 +1,61 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  subject { 
-    described_class.new(question: "What type of animal is a dog?", answer: "mammal",
-                        difficulty_level: 1) 
-  }
+  subject(:question) { 
+      described_class.new(question: "What is x in x + 1 = 2", answer: "1",
+                          difficulty_level: 1) 
+  }  
 
-  it "is valid with valid attributes" do
-    expect(subject).to be_valid
+  describe 'validations' do 
+
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    it "is invalid without a question" do
+      subject.question = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is invalid without an answer" do
+      subject.answer = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is invalid without a difficulty level" do
+      subject.difficulty_level = nil
+      expect(subject).to_not be_valid
+    end
   end
 
-  it "is invalid without a question" do
-    subject.question = nil
-    expect(subject).to_not be_valid
+  describe 'is_correct?' do
+    context 'when answer is correct' do
+      it 'returns true' do
+        expect(subject.is_correct?(question.answer)).to be true
+      end
+    end
+
+    context 'when answer is incorrect' do
+
+      it 'returns false' do
+        expect(subject.is_correct?("2")).to be false
+      end
+    end
+
+    context 'when answer is a number' do
+      context 'and is correct but in words' do
+        it 'returns true' do
+          expect(subject.is_correct?("one")).to be true
+        end
+      end
+
+      context 'and is incorrect but in words' do
+        it 'returns false' do
+          expect(subject.is_correct?("two")).to be false
+        end
+      end
+    end
+
   end
 
-  it "is invalid without an answer" do
-    subject.answer = nil
-    expect(subject).to_not be_valid
-  end
-
-  it "is invalid without a difficulty level" do
-    subject.difficulty_level = nil
-    expect(subject).to_not be_valid
-  end
 end
