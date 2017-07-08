c01bd3002901c98fa8bf81a84ed397129563bd02
Nina <ninariccimarie@gmail.com>
2017-07-08 21:04:56 +0800

    add data with different difficulty levels

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,6 +2,14 @@ FactoryGirl.define do
   factory :question do
     question "What type of animal is a dog?"
     answer "Mammal"
+    difficulty_level 0
+  end
+
+  factory :question_medium, parent: :question do
     difficulty_level 1
+  end
+
+ factory :question_hard, parent: :question do
+    difficulty_level 2
   end
 end
