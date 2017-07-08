d98bcc64ba3c6aafb24fb774a9872985b887f07f
Nina <ninariccimarie@gmail.com>
2017-07-08 17:18:03 +0800

    add create and show actions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,30 @@
 class QuestionsController < ApplicationController
     before_action :set_question, only: [:show, :update, :destroy]
 
-    #GET /questions/1.json
+    #GET /questions
     def index
         @questions = Question.all
         json_response(@questions)
+    end
+
+    #GET /questions/:id
+    def show
+        json_response(@question)
+    end
+
+    #POST /questions
+    def create
+        @question = Question.create!(question_params)
+        json_response(@question, :created)
+    end
+
+    private
+
+    def question_params
+        params.permit(:question, :answer, :difficulty_level)
+    end
+
+    def set_question
+        @question = Question.find(params[:id])
     end
 end
