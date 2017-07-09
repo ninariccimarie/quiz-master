74c5a2fa101165ee6766c408ba7bd62c5a9219ab
Nina <ninariccimarie@gmail.com>
2017-07-09 15:25:54 +0800

    initial front-end

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {
+	BrowserRouter as Router,
+	Route,
+	Link
+} from 'react-router-dom';
+
+import styles from './app.scss'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
