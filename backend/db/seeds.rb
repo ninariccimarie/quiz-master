6ddc8071104fe0709e7b0ccaed29991144b4411b
Nina <ninariccimarie@gmail.com>
2017-07-09 22:19:09 +0800

    add data seeder

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+question_list = [
+    ['What sweet food made by bees using nectar from flowers?', 'Honey', 'easy'],
+    ['Name the school that Harry Potter attended?', 'Hogwarts', 'medium'],
+    ['What\'s the capital of Zimbabwe?', 'Harare', 'hard' ]
+]
+
+question_list.each do |question, answer, difficulty_level|
+    Question.create(question: question, answer: answer, difficulty_level: difficulty_level)
+end
