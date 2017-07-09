9edd00232c825c6aa394825146f9f19b68c874bc
Nina <ninariccimarie@gmail.com>
2017-07-09 12:46:02 +0800

    edit is_correct? method to pass words test case

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,9 +7,9 @@ class Question < ApplicationRecord
     def is_correct?(user_answer)
 
         if is_integer?(answer)
-            NumbersInWords.in_numbers(user_answer) == answer.to_i
+            NumbersInWords.in_numbers(user_answer) == answer.to_i           
         else
-            answer == user_answer
+            self.answer.downcase == user_answer.strip.downcase
         end
     end
 
