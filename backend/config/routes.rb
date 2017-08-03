51953ec4fd0a9cc6e7aa57ec2a0a457bb1a8a11b
Robbie Marcelo <rbmrclo@hotmail.com>
2017-08-03 15:42:58 +0800

    Move questions resources under api/v1 namespace

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    resources :questions, only: [:index, :create, :show, :update, :destroy] do
-      member do
-        get :answers
+    namespace :v1 do
+      resources :questions, only: [:index, :create, :show, :update, :destroy] do
+        member do
+          get :answers
+        end
       end
     end
   end
