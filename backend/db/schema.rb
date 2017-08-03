620106835fb0383c4200a02627a3f7967b5ce6ca
Nina Ricci Marie <ninariccimarie@gmail.com>
2017-08-03 10:48:23 +0800

    add name on clients table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 20170801101708) do
 
   create_table "clients", force: :cascade do |t|
+    t.string "name"
     t.string "token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
