e0b5d364479da8f11d792a2b61fac891e21b3ecd
Nina Ricci Marie <ninariccimarie@gmail.com>
2017-08-03 18:53:39 +0800

    Edit authentication to pass token via headers

--- backend/app/controllers/api/api_controller.rb
+++ backend/app/controllers/api/api_controller.rb
@@ -7,10 +7,16 @@ class Api::ApiController < ApplicationController
     #
     #   Create client model
     #
-    token = Client.find_by(token: params[:token])
+    # token = Client.find_by(token: params[:token])
+    # puts 'api_controller client token'
+    # puts token
 
-    unless Rails.application.secrets.whitelisted_token == token
-      json_response({ errors: [ { detail: "Access denied" } ] }, 401)
+    # unless Rails.application.secrets.whitelisted_token == token
+    #   json_response({ errors: [ { message: "Access denied" } ] }, 401)
+    # end
+
+    authenticate_or_request_with_http_token do |token, options|
+      Client.exists?(token: token)
     end
   end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,19 +1,11 @@
+Client.create(name: 'Nina', token: 'WLXTomWa3mM8QKtKRe5W2Mr2')
+
 question_list = [
   ['What sweet food made by bees using nectar from flowers?', 'Honey', 'easy'],
   ['Name the school that Harry Potter attended?', 'Hogwarts', 'medium'],
   ['What\'s the capital of Zimbabwe?', 'Harare', 'hard' ]
 ]
 
-token_list = [
-  ['dfYGLxuSEbGW6gh65KUX2Qna'],
-  ['vE9M8DMiJYqEHbduqX2huPYJ'],
-  ['s7XsPnTuPMJRwytf3vnNw7GA']
-]
-
 question_list.each do |question, answer, difficulty_level|
   Question.create(question: question, answer: answer, difficulty_level: difficulty_level)
-end
-
-token_list.each do |token|
-  Client.create(token: token)
 end
