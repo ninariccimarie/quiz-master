fea8fb94859ecac867bc3d4457b7244fbd14624e
Robbie Marcelo <rbmrclo@hotmail.com>
2017-08-03 15:43:17 +0800

    Migrate client model to database

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170801101708) do
+ActiveRecord::Schema.define(version: 20170803071158) do
 
   create_table "clients", force: :cascade do |t|
-    t.string "name"
     t.string "token"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
