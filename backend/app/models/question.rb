48b2b1528d0728f50a31205c756ca970f0168bdc
Nina Ricci Marie <ninariccimarie@gmail.com>
2017-08-04 16:07:41 +0800

    follow spaces convention rule

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -1,26 +1,25 @@
 require 'numbers_in_words'
 
 class Question < ApplicationRecord
-    enum difficulty_level: [:easy, :medium, :hard]
-    validates_presence_of :question, :answer, :difficulty_level
+  enum difficulty_level: [:easy, :medium, :hard]
+  validates_presence_of :question, :answer, :difficulty_level
 
-    def is_correct?(user_answer)
-
-        if is_integer?(answer)
-            NumbersInWords.in_numbers(user_answer) == answer.to_i
-        else
-            self.answer.downcase == user_answer.strip.downcase
-        end
+  def is_correct?(user_answer)
+    if is_integer?(answer)
+        NumbersInWords.in_numbers(user_answer) == answer.to_i
+    else
+        self.answer.downcase == user_answer.strip.downcase
     end
+  end
 
-    def self.filter(filter)
-        if filter
-            where(difficulty_level: filter)
-        end
+  def self.filter(filter)
+    if filter
+        where(difficulty_level: filter)
     end
+  end
 
-    private
-    def is_integer?(string)
-        /\A[-+]?\d+\z/.match(string)
-    end
+  private
+  def is_integer?(string)
+    /\A[-+]?\d+\z/.match(string)
+  end
 end
