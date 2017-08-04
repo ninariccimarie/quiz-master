2b70b7febc71fe55cfc7dbd3d7f00c596d57e9c6
Nina Ricci Marie <ninariccimarie@gmail.com>
2017-08-04 16:04:55 +0800

    remove comments

--- backend/app/controllers/api/api_controller.rb
+++ backend/app/controllers/api/api_controller.rb
@@ -3,18 +3,6 @@ class Api::ApiController < ApplicationController
   before_action :authenticate!
 
   def authenticate!
-    # TODO:
-    #
-    #   Create client model
-    #
-    # token = Client.find_by(token: params[:token])
-    # puts 'api_controller client token'
-    # puts token
-
-    # unless Rails.application.secrets.whitelisted_token == token
-    #   json_response({ errors: [ { message: "Access denied" } ] }, 401)
-    # end
-
     authenticate_or_request_with_http_token do |token, options|
       Client.exists?(token: token)
     end
