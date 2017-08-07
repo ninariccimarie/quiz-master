e6344a17043ff51360cbdeac28129db9c1de3547
Nina Ricci Marie <ninariccimarie@gmail.com>
2017-08-07 15:13:59 +0800

    remove puts, edit routes, use json helper method

--- backend/spec/requests/questions_spec.rb
+++ backend/spec/requests/questions_spec.rb
@@ -5,12 +5,12 @@ RSpec.describe "Questions API", type: :request do
   let!(:questions_medium) { create_list(:question_medium, 3) }
   let!(:questions_hard) { create_list(:question_hard, 2) }
   let(:question_id) { questions.first.id }
+  let(:token) {'WLXTomWa3mM8QKtKRe5W2Mr2'}
 
-  describe 'GET /api/questions' do
-    before { get '/api/questions?' }
-        
+  describe 'GET /api/v1/questions' do
+    before { get '/api/v1/questions' }
+
     it 'retrieves questions' do
-      puts json
       expect(json).not_to be_empty
       expect(json.size).to eq(10)
     end
@@ -21,7 +21,7 @@ RSpec.describe "Questions API", type: :request do
   end
 
   describe 'GET /api/questions/:id' do
-    before { get "/api/questions/#{question_id}" }
+    before { get "/api/v1/questions/#{question_id}" }
 
     context 'when the question exists' do
       it "returns the question" do
@@ -47,15 +47,15 @@ RSpec.describe "Questions API", type: :request do
     end
 end
 
-  describe 'POST /questions' do
+  describe 'POST /api/v1/questions' do
     let(:valid_attributes) { { question: 'What type of animal is a dog?', answer: 'Mammal',
                                difficulty_level: 'easy' } }
 
     context 'when the request is valid' do
-      before { post '/questions', params: valid_attributes }
+      before { post '/api/v1/questions', params: valid_attributes }
 
       it 'creates a question' do
-        expect(JSON.parse(response.body)['answer']).to eq('Mammal')
+        expect(json)['answer'].to eq('Mammal')
       end
 
       it 'responds status code 201' do
@@ -71,17 +71,16 @@ end
       end
 
       it 'returns a validation error message' do
-        expect(response.body)
-            .to match(/Validation failed: Question can't be blank, Difficulty level can't be blank/)
+        expect(response.body).to match(/Validation failed: Question can't be blank, Difficulty level can't be blank/)
       end
     end
   end
 
-  describe 'PUT /questions/:id' do
+  describe 'PUT /api/v1/questions/:id' do
     let(:valid_attributes) { { difficulty_level: 'medium' } }
 
     context 'when the record exists' do
-      before { put "/questions/#{question_id}", params: valid_attributes}
+      before { put "/api/v1/questions/#{question_id}", params: valid_attributes}
 
       it 'updates the question' do
         expect(response.body).to be_empty
@@ -93,55 +92,54 @@ end
     end
   end
 
-  describe 'DELETE /questions/:id' do
-    before { delete "/questions/#{question_id}" }
+  describe 'DELETE /api/v1/questions/:id' do
+    before { delete "/api/v1/questions/#{question_id}" }
 
     it 'returns status code 204' do
       expect(response).to have_http_status(204)
     end
   end
 
-  describe 'GET /questions?difficulty_level=:difficulty_level' do
+  describe 'GET /api/v1/questions?difficulty_level=:difficulty_level' do
     let(:easy) { { difficulty_level: 'easy' } }
     let(:medium) { { difficulty_level: 'medium' } }
     let(:hard) { { difficulty_level: 'hard' } }
 
     context 'when difficulty level: easy' do
-      before { get '/questions?difficulty_level=easy' }
+      before { get '/api/v1/questions?difficulty_level=easy' }
 
       it 'filters list of questions with difficulty level: easy' do
-        expect(JSON.parse(response.body)).not_to be_empty
-        expect(JSON.parse(response.body).size).to eq(5)
+        expect(json).not_to be_empty
+        expect(json).size.to eq(5)
       end
     end
 
     context 'when difficulty level: medium' do
-      before { get '/questions?difficulty_level=medium' }
+      before { get '/api/v1/questions?difficulty_level=medium' }
 
       it 'filters list of questions with difficulty level: medium' do
-        expect(JSON.parse(response.body)).not_to be_empty
-        expect(JSON.parse(response.body).size).to eq(3)
+        expect(json).not_to be_empty
+        expect(json).size.to eq(3)
       end
     end
 
     context 'when difficulty level: hard' do
-      before { get '/questions?difficulty_level=hard' }
+      before { get '/api/v1/questions?difficulty_level=hard' }
 
       it 'filters list of questions with difficulty level: hard' do
-        expect(JSON.parse(response.body)).not_to be_empty
-        expect(JSON.parse(response.body).size).to eq(2)
+        expect(json).not_to be_empty
+        expect(json).size.to eq(2)
       end
     end
   end
-
+  
   describe 'POST /questions/:id' do
 
     context 'when user anwer is correct' do
       before { post '/questions/1', params: { user_answer: 'Mammal' } }
 
       it 'returns a correct answer message' do
-        expect(response.body)
-            .to match(/Yay! You're correct!/)
+        expect(response.body).to match(/Yay! You're correct!/)
       end
     end
 
@@ -149,8 +147,7 @@ end
       before { post '/questions/1', params: { user_answer: 'Reptile' } }
 
       it 'returns a wrong answer message' do
-        expect(response.body)
-            .to match(/Sorry, your answer is wrong. \:\(/)
+        expect(response.body).to match(/Sorry, your answer is wrong. \:\(/)
       end
     end
   end
