b297105157193a02b0be3df29f0cbcf64cc8e112
Robbie Marcelo <rbmrclo@hotmail.com>
2017-08-07 16:34:01 +0800

    Refactor filter

--- backend/app/models/question.rb
+++ backend/app/models/question.rb
@@ -2,17 +2,13 @@ class Question < ApplicationRecord
   enum difficulty_level: [:easy, :medium, :hard]
   validates_presence_of :question, :answer, :difficulty_level
 
+  scope :filter, ->(filter) { where(difficulty_level: filter) }
+
   def is_correct?(user_answer)
     if is_integer?(answer)
       NumbersInWords.in_numbers(user_answer) == answer.to_i
     else
       self.answer.downcase == user_answer.strip.downcase
-    end
-  end
-
-  def self.filter(filter)
-    if filter
-      where(difficulty_level: filter)
     end
   end
 
