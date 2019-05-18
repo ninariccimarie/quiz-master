de32985cebd6882b3b81a74283f60413d8138af1
Nina Ricci Benite <ninariccimarie@gmail.com>
2019-05-18 08:45:19 +0800

    add quizzes seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -12,3 +12,58 @@ question_list = [
 question_list.each do |question, answer, difficulty_level|
   Question.create(question: question, answer: answer, difficulty_level: difficulty_level)
 end
+
+quizzes = [
+  ["Marlon", "Sampaguita", "Araling Panlipuan", "Monday",
+    [
+      {
+          "question_id": 1,
+          "answer": "honey"
+      },
+      {
+          "question_id": 2,
+          "answer": "asadadsa"
+      },
+      {
+          "question_id": 3,
+          "answer": "asadsa"
+      }
+    ]
+  ],
+  ["Caras", "Sampaguita", "Science", "Tuesday",
+    [
+      {
+          "question_id": 1,
+          "answer": "honey"
+      },
+      {
+          "question_id": 2,
+          "answer": "hello"
+      },
+      {
+          "question_id": 3,
+          "answer": "hi"
+      }
+    ]
+  ],
+  ["Nina", "Crysanthemum", "Science", "Tuesday",
+    [
+      {
+          "question_id": 1,
+          "answer": "honey"
+      },
+      {
+          "question_id": 2,
+          "answer": "syrup"
+      },
+      {
+          "question_id": 3,
+          "answer": "hasdsai"
+      }
+    ]
+  ],
+]
+
+quizzes.each do |student_name, class_name, subject, day, answers|
+  Quiz.create(student_name: student_name, class_name: class_name, subject: subject, day: day, answers: answers)
+end
